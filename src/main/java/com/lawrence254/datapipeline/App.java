0a2cb8aa8a6e984008fec286c909d9ac92fe1392
lawrence254 <lawrenslava1@gmail.com>
2025-01-22 21:11:04 +0300

    Instantiate the modular datapipeline project

---
+++ src/main/java/com/lawrence254/datapipeline/App.java
@@ -0,0 +1,10 @@
+package com.lawrence254.datapipeline;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
