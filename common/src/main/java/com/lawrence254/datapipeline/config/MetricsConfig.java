b766cd535a9b0a33524d91b01eea4cb25dfb108f
lawrence254 <lawrenslava1@gmail.com>
2025-02-04 12:30:00 +0300

    Add a bean for MeterRegistry in the config file

--- common/src/main/java/com/lawrence254/datapipeline/config/MetricsConfig.java
+++ common/src/main/java/com/lawrence254/datapipeline/config/MetricsConfig.java
@@ -2,6 +2,8 @@ package com.lawrence254.datapipeline.config;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,10 @@ public class MetricsConfig {
     @Bean
     public TimedAspect timedAspect(MeterRegistry registry) {
         return new TimedAspect(registry);
+    }
+
+    @Bean
+    public MeterRegistry meterRegistry() {
+        return new SimpleMeterRegistry();
     }
 }
